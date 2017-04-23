7c4dc945ac3851ac1781716793035d65d8026b6a
Oksana <oxana.dychok@gmail.com>
2017-04-23 20:52:30 +0300

    Функції віднімання, множення і ділення

--- CalcClass/MathOperations.cs
+++ CalcClass/MathOperations.cs
@@ -29,5 +29,55 @@ namespace CalcClass
             }
             return result;
         }
+        public static int Sub(long a, long b)
+        {
+            int result;
+            try
+            {
+                result = (int)(a - b);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            return result;
+        }
+
+
+        public static int Mult(long a, long b)
+        {
+            int result;
+            try
+            {
+                result = (int)(a * b);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            return result;
+        }
+
+        public static int Div(long a, long b)
+        {
+            int result;
+            try
+            {
+                result = (int)(a / b);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            catch (DivideByZeroException)
+            {
+                _lastError = "Error 09";
+                result = 0;
+            }
+            return result;
+        }
     }
 }
